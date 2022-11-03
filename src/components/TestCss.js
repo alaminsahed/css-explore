fdcbccfe1a64541e88f686cca750ac52b449c5a6
alaminsahed <alaminsahed97@gmail.com>
2022-11-03 18:17:59 +0600

    feat: left part css

--- src/components/TestCss.js
+++ src/components/TestCss.js
@@ -6,7 +6,7 @@ const TestCss = () => {
             <section className="relative overflow-hidden">
                 <div style={{ opacity: 1, transform: "none" }}>
                     <span className=" bg-gradient-to-tr from-[#e32c6b] to-[#288cf9] w-full absolute white h-[1px] top-0 opacity-25"></span>
-                    <span className="bg-gradient-180  from-[#58a5ff] to-[#a67af4] left-[-99px] absolute w-[28rem] h-96 top-[-227px]  rounded-full mix-blend-normal blur-2xl   dark:opacity-35 opacity-[0.15]"></span>
+                    <span className="bg-gradient-180  from-[#58a5ff] to-[#a67af4] left-[-99px] absolute w-[44rem] h-96 top-[-227px]  rounded-full mix-blend-normal blur-2xl   dark:opacity-35 opacity-[0.15]"></span>
                     <span className=" dark:opacity-35 opacity-[0.15]"></span>
                 </div>
                 <section className="relative flex flex-col items-center px-6 py-16 font-sans md:py-24 lg:py-32 gap-14">
